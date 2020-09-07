fc13c7a4b81d2c0035fc731f3635c32edbfb8f72
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-09-06 20:25:41 -0400

    Changing config for heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
+var handlebars = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 var PORT = process.env.PORT || 8080;
@@ -8,8 +8,10 @@ var app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 app.use(routes);
 
 app.listen(PORT, function() {
